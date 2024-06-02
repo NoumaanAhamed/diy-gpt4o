ba78008286aa5fc64f404c6d29a228a23dbc1bd8
NoumaanAhamed <noumaanahamed@gmail.com>
2024-06-02 20:19:31 +0530

    text version done

--- main.py
+++ main.py
@@ -1,28 +1,72 @@
 from dotenv import load_dotenv
 from groq import Groq
 import os
-from PIL import ImageGrab
+from PIL import ImageGrab,Image
 import cv2
 import pyperclip
+import google.generativeai as genai
 
 load_dotenv()
 
 groq_client = Groq(
     api_key=os.getenv("GROQ_API_KEY"),
 )
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 web_cam = cv2.VideoCapture(0)
 
-def get_response_from_groq(message,model="llama3-8b-8192"):
-    conversation = [{
-        "role": "user",
-        "content": message,
-    }]
+sys_msg = ( 
+    'You are a multi-modal AI voice assistant. Your user may or may not have attached a photo for context '
+    '(either a screenshot or a webcam capture). Any photo has already been processed into a highly detailed '
+    'text prompt that will be attached to their transcribed voice prompt. Generate the most useful and '
+    'factual response possible, carefully considering all previous generated text in your response before '
+    'adding new tokens to the response. Do not expect or request images, just use the context if added. '
+    'Use all of the context of this conversation so your response is relevant to the conversation. Make '
+    'your responses clear and concise, avoiding any verbosity.'
+)
+
+conversation = [{"role":"system","content":sys_msg}]
+
+generation_config = {
+    'temperature': 0.7,
+    'top_p': 1,
+    'top_k': 1,
+    'max_output_tokens':2048
+}
+
+safety_settings = [
+{
+'category': 'HARM_CATEGORY_HARASSMENT',
+'threshold': 'BLOCK_NONE'
+},
+{
+'category': 'HARM_CATEGORY_HATE_SPEECH', 
+'threshold': 'BLOCK_NONE'
+},
+{
+'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+'threshold': 'BLOCK_NONE'
+},
+{
+'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
+'threshold': 'BLOCK_NONE'
+},
+]
+
+model = genai.GenerativeModel('gemini-1.5-flash',
+                              safety_settings=safety_settings,
+                              generation_config=generation_config)
+
+def get_response_from_groq(message,img_context,model="llama3-8b-8192"):
+    if img_context:
+        message = f'USER PROMPT: {message}\n\n  IMAGE CONTEXT: {img_context}'
+    conversation.append({'role': 'user','content': message})
     chat_completion = groq_client.chat.completions.create(
         messages=conversation,
         model=model,
     )
-    reponse = chat_completion.choices[0].message
-    return reponse.content
+    response = chat_completion.choices[0].message
+    conversation.append(response)
+    return response.content
 
 
 def determine_action(prompt):
@@ -71,7 +115,7 @@ def take_screenshot():
         screenshot = ImageGrab.grab()
         rgb_screenshot = screenshot.convert("RGB")
         rgb_screenshot.save(path,quality=15)
-        return path
+        return 'Screenshot Taken'
     except Exception as e:
         return f"An error occurred: {e}"
 
@@ -92,13 +136,40 @@ def get_clipboard_content():
         print("The clipboard content is not a text.")
         return None
 
+def vision_prompt(prompt, image_path):
+    img = Image.open(image_path)
+    prompt = (
+        'You are the vision analysis AI that provides semtantic meaning from images to provide context '
+        'to send to another AI that will create a response to the user. Do not respond as the AI assistant '
+        'to the user. Instead take the user prompt input and try to extract all meaning from the photo '
+        'relevant to the user prompt. Then generate as much objective data about the image for the AI ' 
+        f'assistant who will respond to the user. \nUSER PROMPT: {prompt}')
+    response = model.generate_content([prompt, img])
+    return response.text
+
 while True:
     user_input = input("Enter your prompt: ")
     if user_input == "exit":
         break
     action = determine_action(user_input)
     print(f'AI Action: {action}')
-    response = get_response_from_groq(user_input)
+    if 'take screenshot' in action:
+        print('Taking Screenshot')
+        take_screenshot()
+        visual_context = vision_prompt(user_input, 'screenshot.jpg')
+    elif 'capture webcam' in action:
+        print('Capturing Webcam')
+        capture_web_cam()
+        visual_context = vision_prompt(user_input, 'webcam.jpg')
+    elif 'extract clipboard' in action:
+        print('Extracting Clipboard Content')
+        paste = get_clipboard_content()
+        user_input = f'{user_input}\n\n CLIPBOARD CONTENT: {paste}'
+        visual_context = None
+    else:
+        visual_context = None
+
+    response = get_response_from_groq(user_input, visual_context)
     print(f'AI Response: {response}')
     
 
